412f38c93467f5d80a572f65ee43d47bf1c0ea16
Konstantin <KonstantKar@yandex.ru>
2023-08-14 23:34:21 +0300

    Адаптивность Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,21 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import Profile from "../../assets/Me.jpg";
+import LittleProfile from "../../assets/LittleScreenMe.jpg";
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import TypedText from "../../components/TypedText/TypedText";
 
 const Home = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    // Обработчик изменения размера окна
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Добавляем обработчик события при монтировании компонента
+    window.addEventListener("resize", handleResize);
+
+    // Убираем обработчик события при размонтировании компонента
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const imageSrc = windowWidth > 1024 ? Profile : LittleProfile;
+
   return (
     <section className="home section grid">
-      <img src={Profile} alt="" className="home__img" />
+      <img src={imageSrc} alt="" className="home__img" />
       <div className="home__content">
         <div className="home__data">
           <h1 className="home__title">
             <span>Я Егор Петров.</span>
             <br />
             <span>
-              <TypedText text={["Режиссёр!", "Оператор!", "Монтажёр!"]} />
+              <TypedText text={["Режиссёр.", "Оператор.", "Монтажёр."]} />
             </span>
           </h1>
           <p className="home__description">
@@ -23,7 +43,7 @@ const Home = () => {
             Ну я люблю доту типо, ну еще в сталкрафте нубиков пострелять, такие
             дела вот.А ДАА! Тинкер = мой любимый герой!
           </p>
-          <Link to="/about" className="button">
+          <Link to="/about" className="button home__button">
             Больше обо мне{" "}
             <span className="button__icon">
               <FaArrowRight />
